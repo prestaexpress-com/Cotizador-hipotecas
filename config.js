6a51f080a2cb3844279ce7895b93711726c5b2b3
cristhianC-ARX <62726642+cristhianC-ARX@users.noreply.github.com>
2026-05-18 21:19:25 -0600

    Update Supabase key in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   SUPABASE_URL: 'https://isqoysjgeibdebttthly.supabase.co',
-  SUPABASE_KEY: 'PEGA_AQUI_TU_PUBLISHABLE_KEY'
+  SUPABASE_KEY: 'sb_publishable_hdVci8cWLRbRHToC8hL8Eg_-_Xr_O_d'
 };
